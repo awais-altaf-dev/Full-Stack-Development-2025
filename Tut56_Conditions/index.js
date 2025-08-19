6545dbf65492cc7d95bba43860467e640e87f204
Muhammad Awais Altaf <168270210+awaisaltaf5@users.noreply.github.com>
2025-08-19 15:33:51 +0500

    Conditional Statements

---
+++ Tut56_Conditions/index.js
@@ -0,0 +1,20 @@
+console.log("So this is about Conditional by Muhammad Awais Altaf! Happy learning")
+let age=17;
+let grace=3;
+age+=grace;
+console.log(age);
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age%grace);
+console.log(age**grace); // exponentiation operator
+
+// Conditional Statements
+if(age>18){
+    console.log("You are eligible to vote.");
+
+}
+else{
+    console.log("You are not eligible to vote.");
+}
