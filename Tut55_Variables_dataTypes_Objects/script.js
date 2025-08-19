8426a3d7cf1354e38e65a2949d67db21bc90ac28
Muhammad Awais Altaf <168270210+awaisaltaf5@users.noreply.github.com>
2025-08-19 13:44:34 +0500

    Tut55_Variable,Data Types, Objects

---
+++ Tut55_Variables_dataTypes_Objects/script.js
@@ -0,0 +1,36 @@
+console.log("Hey I am Doing code of java script of Turtorial 55")
+var a = 10;
+var b = 5;
+var c = "Muhamamd Awais Altaf"
+console.log(a + b);
+console.log(typeof a, typeof b, typeof c);
+// {
+//     let a=20;
+//     console.log(a); Block Scope
+// }
+// {
+//     var a=20;
+//     // console.log(a); //Global Scope
+// }
+let x = "Muhammad Awais ALtaf";//String
+let y = 45;//Number
+let p = 3.34;//Number
+const h = true;//Boolean
+let z = undefined;//Undefined
+let i = null;//Object 
+console.log(x, y, p, h, z, i);
+console.log(typeof x, typeof y, typeof p, typeof h, typeof z, typeof i);
+
+//Making Object
+
+let person = {
+    name: "Muhammad Awais Altaf",
+    age: 20,
+    "job position": "Senerior Software Engineer",
+    salary: 200000,
+    isMarried: false
+}
+console.log(person);
+person.salary = 250000;
+console.log(person);
+console.log(person.salary);
