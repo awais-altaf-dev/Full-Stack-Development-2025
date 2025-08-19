0ac5c3f4e6485b8421e195051955302e5bc9652b
Muhammad Awais Altaf <168270210+awaisaltaf5@users.noreply.github.com>
2025-08-19 16:01:29 +0500

    Conditional Statements in Java script

--- Tut56_Conditions/index.js
+++ Tut56_Conditions/index.js
@@ -23,3 +23,14 @@ else{
 //Ternary Operator
 var c=a>b?(a-b):(b-a);
 console.log(c);
+// Switch Statement
+switch (age) {
+    case 18:
+        console.log("You just became eligible to vote.");
+        break;
+    case 19:
+        console.log("You are still eligible to vote.");
+        break;
+    default:
+        console.log("You are not eligible to vote.");
+}
